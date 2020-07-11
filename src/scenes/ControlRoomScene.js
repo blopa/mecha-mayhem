b9b415444b62f49246cf197565945a0b9399b5f1
Shen Sat <shensat@shens-mbp.home>
2020-07-11 10:41:15 +0100

    Disable camera, add chargeLaser button, add rects for control room background and rooms

--- src/scenes/ControlRoomScene.js
+++ src/scenes/ControlRoomScene.js
@@ -1,6 +1,6 @@
 import { Scene } from 'phaser';
 import Hero from '../sprites/Hero';
-import Background from '../sprites/Background';
+// import Background from '../sprites/Background';
 
 class ControlRoomScene extends Scene {
     constructor() {
@@ -12,13 +12,13 @@ class ControlRoomScene extends Scene {
     }
 
     create() {
-        this.background = new Background({
-            scene: this,
-            x: 0,
-            y: 0,
-            asset: 'control_room',
-        }).setOrigin(0, 0);
-        this.add.existing(this.background);
+        // this.background = new Background({
+        //     scene: this,
+        //     x: 0,
+        //     y: 0,
+        //     asset: 'control_room',
+        // }).setOrigin(0, 0);
+        // this.add.existing(this.background);
 
         this.hero = new Hero({
             scene: this,
@@ -28,12 +28,24 @@ class ControlRoomScene extends Scene {
         });
         this.add.existing(this.hero);
 
-        this.cameras.main.startFollow(this.hero);
         this.scene.launch('RobotStageScene');
+
+        //Shen stuff
+        this.chargeLaserButton = this.input.keyboard.addKey('A');
+        this.controlRoomBackground = new Phaser.Geom.Rectangle(0, 0, 1000, 1000);
+        this.controlRoomBackgroundFill = this.add.graphics({ fillStyle: { color: 0x741B47 } });
+        this.controlRoomBackgroundFill.fillRectShape(this.controlRoomBackground);
+        this.laserRoom = new Phaser.Geom.Rectangle(250, 200, 50, 50);
+        this.laserRoomFill = this.add.graphics({ fillStyle: { color: 0xC27BA0 } });
+        this.laserRoomFill.fillRectShape(this.laserRoom);
     }
 
     update(time, delta) {
         this.hero.update(time, delta);
+        if (this.chargeLaserButton.isDown) {
+            console.log('Charging...');
+        }
+        this.dummy = this.hero.getBounds();
     }
 }
 
