db8711649389efda9056eed509df9a131635e08d
Shen Sat <shensat@shens-mbp.home>
2020-07-11 13:28:32 +0100

    Add charge bar to laser room

--- src/scenes/ControlRoomScene.js
+++ src/scenes/ControlRoomScene.js
@@ -32,6 +32,7 @@ class ControlRoomScene extends Scene {
 
         // Shen stuff
         this.chargeLaserButton = this.input.keyboard.addKey('A');
+
         this.controlRoom = new Phaser.Geom.Rectangle(0, 0, 1000, 1000);
         this.controlRoomFill = this.add.graphics({ fillStyle: { color: 0x741B47 } });
         this.controlRoomFill.fillRectShape(this.controlRoom);
@@ -39,18 +40,26 @@ class ControlRoomScene extends Scene {
         this.physics.world.enable(this.hero);
         this.physics.world.enable(this.laserRoom);
 
-        this.physics.add.overlap(this.hero, this.laserRoom, this.inLaserRoom);
+        this.laserChargeCounter = 0;
+        this.laserChargeLimit = 1;
+
+        this.laserChargeBar = this.add.rectangle(this.laserRoom.x + 25 + 5, this.laserRoom.y + 25, 10, 0);
+        this.laserChargeBar.setFillStyle(0x00FF00);
     }
 
     update(time, delta) {
         this.hero.update(time, delta);
-        if (this.chargeLaserButton.isDown) {
+        if (this.laserChargeCounter >= this.laserChargeLimit) {
+            console.log('Laser ready!')
+        } else if (this.physics.overlap(this.hero, this.laserRoom) && (this.chargeLaserButton.isDown)) {
+            this.laserChargeCounter += 0.01;
+            this.laserChargeBar.height = this.laserRoom.height * (this.laserChargeCounter/this.laserChargeLimit) * - 1;
             console.log('Charging...');
         }
     }
 
-    inLaserRoom() {
-        console.log('In room!');
+    foobar() {
+        return this.laserChargeCounter;
     }
 }
 
