91c1343b5be0bd3491bf834fc67abb3babec691d
Shen Sat <shensat@shens-mbp.home>
2020-07-11 19:10:45 +0100

    Resolve conflicts

--- src/scenes/ControlRoomScene.js
+++ src/scenes/ControlRoomScene.js
@@ -14,16 +14,25 @@ class ControlRoomScene extends Scene {
     create() {
         this.laserRoom = new Room({
             scene: this,
-            x: 450,
-            y: 250,
+            x: 600,
+            y: 350,
             asset: 'room',
         });
+
+        this.punchRoom = new Room({
+            scene: this,
+            x: 400,
+            y: 200,
+            asset: 'room',
+        });
+
         this.add.existing(this.laserRoom);
+        this.add.existing(this.punchRoom);
 
         this.hero = new Hero({
             scene: this,
-            x: 100,
-            y: 250,
+            x: 400,
+            y: 300,
             asset: 'hero',
         });
         this.add.existing(this.hero);
@@ -32,6 +41,7 @@ class ControlRoomScene extends Scene {
 
         // Shen stuff
         this.chargeLaserButton = this.input.keyboard.addKey('A');
+        this.chargePunchButton = this.input.keyboard.addKey('S');
 
         this.controlRoom = new Phaser.Geom.Rectangle(0, 0, 1000, 1000);
         this.controlRoomFill = this.add.graphics({ fillStyle: { color: 0x4C1130 } });
@@ -42,16 +52,19 @@ class ControlRoomScene extends Scene {
 
         this.laserChargeCounter = 0;
         this.laserChargeLimit = 1;
-        this.laserText = this.add.text(350, 300, '');
-
         this.laserChargeBar = this.add.rectangle(this.laserRoom.x + 25 + 5, this.laserRoom.y + 25, 10, 0);
         this.laserChargeBar.setFillStyle(0x00FF00);
+
+        this.punchChargeCounter = 0;
+        this.punchChargeLimit = 1;
+        this.punchChargeBar = this.add.rectangle(this.punchRoom.x + 25 + 5, this.punchRoom.y + 25, 10, 0);
+        this.punchChargeBar.setFillStyle(0x00FF00);
+
+        this.text = this.add.text(275, 300, '');
 
         this.wall = this.add.rectangle(this.laserRoom.x - 25 + 25, this.laserRoom.y + 25 + 5 + 10, 50, 10).setFillStyle(0x741B47);
 
         this.physics.world.enable(this.wall, 1);
-
-        // this.wallGroup = this.physics.add.staticGroup(); - if we need a group for later
 
         this.physics.add.collider(this.hero, this.wall);
 
@@ -64,15 +77,26 @@ class ControlRoomScene extends Scene {
 
     update(time, delta) {
         this.hero.update(time, delta);
-        this.laserText.text = '';
+        this.text.text = '';
+        //arm laser
         if (this.laserChargeCounter >= this.laserChargeLimit) {
-            this.laserText.text = 'Laser ready!';
+            this.text.text = 'Laser ready!';
         } else if (this.physics.overlap(this.hero, this.laserRoom) && (this.chargeLaserButton.isDown)) {
             this.laserChargeCounter += 0.01;
             this.laserChargeBar.height = this.laserRoom.height * (this.laserChargeCounter / this.laserChargeLimit) * -1;
-            this.laserText.text = 'Charging...';
+            this.text.text = 'Charging...';
         } else if (this.physics.overlap(this.hero, this.laserRoom)) {
-            this.laserText.text = 'Hold A to charge laser';
+            this.text.text = 'Hold A to charge laser';
+        }
+        //arm punch
+        if (this.punchChargeCounter >= this.punchChargeLimit) {
+            this.text.text = 'Punch ready!';
+        } else if (this.physics.overlap(this.hero, this.punchRoom) && (this.chargepunchButton.isDown)) {
+            this.punchChargeCounter += 0.01;
+            this.punchChargeBar.height = this.punchRoom.height * (this.punchChargeCounter / this.punchChargeLimit) * -1;
+            this.text.text = 'Charging...';
+        } else if (this.physics.overlap(this.hero, this.punchRoom)) {
+            this.text.text = 'Mash S to charge laser';
         }
     }
 }
