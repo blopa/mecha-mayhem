5f9765db88d6540a1bf72a1ac483b172e32da1e5
Pablo Montenegro <montenegro.pablo@outlook.com>
2020-07-12 04:22:53 +0200

    add dino animation

--- src/sprites/Dino.js
+++ src/sprites/Dino.js
@@ -12,7 +12,47 @@ class Dino extends GameObjects.Sprite {
     }) {
         super(scene, x, y, asset, frame);
         this.setDepth(DINO_DEPTH);
+        this.createAnimations();
+        this.setAnimation('idle');
         this.spriteKey = spriteKey;
+    }
+
+    createAnimations = () => {
+        const assetKey = this.texture.key;
+        if (!this.scene.anims.exists(`${assetKey}_idle`)) {
+            this.scene.anims.create({
+                key: `${assetKey}_idle`,
+                frames: this.scene.anims.generateFrameNames(assetKey, {
+                    frames: [
+                        'dino_idle_01',
+                        'dino_idle_02',
+                        'dino_idle_03',
+                        'dino_idle_04',
+                        'dino_idle_05',
+                        'dino_idle_06',
+                    ],
+                }),
+                frameRate: 1,
+                // yoyo: true,
+                repeat: -1,
+            });
+        }
+    };
+
+    setAnimation = (animationName) => {
+        if (this.currentAnimationName === animationName) {
+            return;
+        }
+
+        const assetKey = this.texture.key;
+        const animationKey = `${assetKey}_${animationName}`;
+        this.currentAnimationName = animationName;
+        this.currentAnimationKey = animationKey;
+        this.anims.play(animationKey);
+    };
+
+    addSoundEffects = () => {
+        this.robotLaser = this.scene.sound.add('robot_laser_sfx');
     }
 }
 
